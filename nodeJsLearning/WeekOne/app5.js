18ba982935426f7db1346c02a4d1b1bffb71d83b
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-04-09 10:35:24 +0530

    SOME CHANGES

---
+++ nodeJsLearning/WeekOne/app5.js
@@ -0,0 +1,13 @@
+const a=4;
+const b=6;
+console.log(a,b,a+b);
+console.log('%s',a);
+console.log('Hi Lauraence');
+console.count('Test'+a);
+console.count('Test'+a);
+console.count('Test'+a);
+console.clear();
+console.log('Hi Laurance');
+console.warn('warn');
+console.info('info');
+console.table([{a:1,b:2},{a:5,b:10}]);
