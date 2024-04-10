47eaad7671bc7e22715ea6227eb603a0608d880b
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-04-10 16:12:04 +0530

    some changes

--- nodeJsLearning/WeekTwo/app8.js
+++ nodeJsLearning/WeekTwo/app8.js
@@ -1,4 +1,14 @@
 console.log(Math.random());
 console.log(Math.random()*100);
 console.log(Math.floor(Math.random()*100));
-console.log(Math.floor(Math.random()*100));
+console.log(Math.ceil(Math.random()*100));
+
+
+for(let i=0;i<100;i++)
+{
+    console.log(ranNum(1,10));
+}
+
+function ranNum(min,max){
+    return Math.floor(Math.random()*max)+min;
+}
