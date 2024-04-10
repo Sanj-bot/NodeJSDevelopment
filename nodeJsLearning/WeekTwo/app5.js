2ab7bbaac963f95f1189b214606236783d1eaba1
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-04-10 10:07:06 +0530

    some changes in javascript

--- nodeJsLearning/WeekTwo/app5.js
+++ nodeJsLearning/WeekTwo/app5.js
@@ -6,3 +6,10 @@ function myFun2(message,name,val)
 };
 myFun2("hello brother","Sanjay Boss",22);
 myFun2("Moye, Moye", "Mimo Boss", 221);
+
+function myfun2(a,b,c){
+    return a+b+c;
+
+};
+let x=myfun2(10,20,20);
+console.log(x);
