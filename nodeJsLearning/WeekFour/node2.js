c5e74976297487fb4b25b7b6c34cca837e084c8d
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-04-12 08:15:09 +0530

    some changes

---
+++ nodeJsLearning/WeekFour/node2.js
@@ -0,0 +1,8 @@
+function test1(){
+    console.log('test1');
+
+}
+test1();
+
+// const test1="hello";
+console.log(module.filename);
---
+++ nodeJsLearning/WeekFour/test1.js
@@ -0,0 +1,19 @@
+function test1(){
+    console.log('test100');
+
+
+}
+const person={
+    first:'Laurence',
+    last:'Sevis'
+}
+
+
+
+//test1();
+
+// const test1="hello";
+//console.log(module.filename);
+
+exports.test1=test1;
+exports.person=person;
