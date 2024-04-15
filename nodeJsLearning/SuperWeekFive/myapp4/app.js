9398a3e89aa97d927c2815be333fdc0c5ae94a05
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-04-15 12:27:14 +0530

    some changes

---
+++ nodeJsLearning/SuperWeekFive/myapp4/app.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const app=express();
+const bop=require('body-parser');
+const port=process.env.PORT||8080;
+
+app.use(bop.urlencoded({extended:true}))
+
+
+
+app.get('/user',(req,res)=>{
+    res.sendFile(__dirname+'/public/index.html');
+
+});
+
+// users is the endpoint
+
+app.post('/users',(req,res)=>{
+
+    console.clear();
+    console.log(req.body);
+    res.send('done');
+
+});
+
+app.listen(port,()=>{
+    console.log(`Ready Listening on ${port}`);
+});
