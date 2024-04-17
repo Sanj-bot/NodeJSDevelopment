a36980cada287e8a34beb9df77a1ecbdb2f62245
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-04-17 20:04:08 +0530

    changes done

---
+++ nodeJsLearning/WeekTwo/TryCatch/trycatch.js
@@ -0,0 +1,18 @@
+function tester(val){
+    try{
+        if(val==undefined) throw "no value!";
+        if(isNaN(val))  throw "not a number!"
+        val=Number(val);
+    }
+    catch(err){
+        console.log(`There was an error ${err}`);
+
+    }
+    finally{
+        console.log(`Done ${val}`);
+    }
+}
+
+tester();
+tester(100);
+tester('Hello');
